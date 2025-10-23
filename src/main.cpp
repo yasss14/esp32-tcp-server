afd80b8aadfad76e44dbaf81ccdca1d3fa8efdf1
Julien Boriasse <julien@boriasse.com>
2025-10-23 08:54:45 -0400

    Fix TCP server (revert back from websocket)

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,122 @@
 #include <Arduino.h>
 #include "secrets.h"
 #include "WiFi.h"
-#include <ESPAsyncWebServer.h>
-#include <AsyncWebSocket.h>
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
-AsyncWebServer server(80);
-AsyncWebSocket ws("/WebSocket");
+const int port = 10000;
+WiFiServer server(port);
 
-void onWebSocketEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-  if (type == WS_EVT_CONNECT) {
-    Serial.println("WebSocket client connected");
-  } else if (type == WS_EVT_DISCONNECT) {
-    Serial.println("WebSocket client disconnected");
-  } else if (type == WS_EVT_DATA) {
-    AwsFrameInfo *info = (AwsFrameInfo*)arg;
-    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-      // Traitement des données WebSocket
-      Serial.printf("Received %u bytes of data: %s\n", len, (const char*)data);
-
-      // Vous pouvez traiter les données ici selon vos besoins
+void handleTCPClient() {
+  WiFiClient client = server.available();
+  uint8_t data[30];
+  if (client)
+  {
+    Serial.println("New client");
+    /* check client is connected */
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        int len = client.read(data, 30);
+        if (len < 30)
+        {
+          data[len] = '\0';
+        }
+        else
+        {
+          data[30] = '\0';
+        }
+        Serial.print("client sent: ");
+        Serial.println((char *)data);
+      }
     }
   }
+}
+
+String translateEncryptionType(wifi_auth_mode_t encryptionType)
+{
+
+  switch (encryptionType)
+  {
+  case (WIFI_AUTH_OPEN):
+    return "Open";
+  case (WIFI_AUTH_WEP):
+    return "WEP";
+  case (WIFI_AUTH_WPA_PSK):
+    return "WPA_PSK";
+  case (WIFI_AUTH_WPA2_PSK):
+    return "WPA2_PSK";
+  case (WIFI_AUTH_WPA_WPA2_PSK):
+    return "WPA_WPA2_PSK";
+  case (WIFI_AUTH_WPA2_ENTERPRISE):
+    return "WPA2_ENTERPRISE";
+  default:  
+    return "UNKNOWN";
+  }
+  return "UNKNOWN";
+}
+
+void scanNetworks()
+{
+
+  int numberOfNetworks = WiFi.scanNetworks();
+
+  // Add delay so the terminal can catch up
+  delay(3000);
+
+  Serial.print("Number of networks found: ");
+  Serial.println(numberOfNetworks);
+
+  for (int i = 0; i < numberOfNetworks; i++)
+  {
+
+    Serial.print("Network name: ");
+    Serial.println(WiFi.SSID(i));
+
+    Serial.print("Signal strength: ");
+    Serial.println(WiFi.RSSI(i));
+
+    Serial.print("MAC address: ");
+    Serial.println(WiFi.BSSIDstr(i));
+
+    Serial.print("Encryption type: ");
+    String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
+    Serial.println(encryptionTypeDescription);
+    Serial.println("-----------------------");
+  }
+}
+
+void connectToNetwork()
+{
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+
+  Serial.println("Connected to network");
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Connexion au WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+  // Print MAC address
+  Serial.println("MCU MAC address: " + WiFi.macAddress());
 
-  // Configuration du serveur WebSocket
-  ws.onEvent(onWebSocketEvent);
-  server.addHandler(&ws);
+  scanNetworks();
+  connectToNetwork();
+
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
 
   server.begin();
 }
 
 void loop() {
   // Votre code principal ici
+  handleTCPClient();
 }
